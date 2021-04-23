c484b0264b9dea207540a22eda938a7f818c7d8c
akshatb112 <50200187+akshatb112@users.noreply.github.com>
2021-04-23 16:20:54 +0530

    Add files via upload

--- Banking/Banking/settings.py
+++ Banking/Banking/settings.py
@@ -73,12 +73,15 @@ WSGI_APPLICATION = 'Banking.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.postgresql',  
+        'NAME': 'Bank',  
+        'USER':'postgres',  
+        'PASSWORD':'1234',  
+        'HOST':'localhost',  
+    }  
+}  
 
 
 # Password validation
