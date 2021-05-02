660058f61828475c80d25b75117e74457ca5bc4d
akshatb112 <50200187+akshatb112@users.noreply.github.com>
2021-05-02 15:57:51 +0530

    Add files via upload

--- Banking/Banking/urls.py
+++ Banking/Banking/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Customer.urls')),
-    #path('1', include('Branch.urls')),
-    
-]
+    path('branch/', include('Branch.urls')),
+    path('account/',include('BankAccount.urls')),    
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
